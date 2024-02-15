66031eb15630dd2f68121c4575a38ff1587b3f0f
Nguyễn An Quân <quannguyenthpttd@gmail.com>
2024-02-15 17:07:03 +0700

    Cap nhat

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,112 @@
+import React, { createContext, useEffect, useState } from "react";
+import all_product from "../Components/Assets/all_product";
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+    let cart = {};
+    for (let index = 0; index < 300 + 1; index++) {
+        cart[index] = 0;
+    }
+    return cart;
+}
+
+// const getDefaultCart = () => {
+//     let cart = {};
+//     for (let index = 0; index < all_product.length + 1; index++) {
+//         cart[index] = 0;
+//     }
+//     return cart;
+// }
+
+const ShopContextProvider = (props) => {
+    const [all_product, setAll_Product] = useState([]);
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}/allproducts`)
+            .then((response) => response.json())
+            .then((data) => setAll_Product(data))
+
+        if (localStorage.getItem('auth-token')) {
+            fetch(`${process.env.REACT_APP_API_URL}/getcart`, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/form-data',
+                    'auth-token': `${localStorage.getItem('auth-token')}`,
+                    'Content-Type': 'application/json',
+                },
+                body: "",
+            })
+                .then((response) => response.json())
+                .then((data) => setCartItems(data));
+        }
+    }, [])
+
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        // console.log(cartItems);
+        if (localStorage.getItem('auth-token')) {
+            fetch(`${process.env.REACT_APP_API_URL}/addtocart`, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/form-data',
+                    'auth-token': `${localStorage.getItem('auth-token')}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ "itemId": itemId }),
+            })
+                .then((response) => response.json())
+                .then((data) => console.log(data));
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+        if (localStorage.getItem('auth-token')) {
+            fetch(`${process.env.REACT_APP_API_URL}/removefromcart`, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/form-data',
+                    'auth-token': `${localStorage.getItem('auth-token')}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ "itemId": itemId }),
+            })
+                .then((response) => response.json())
+                .then((data) => console.log(data));
+        }
+    }
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = all_product.find((product) => product.id === Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
+
+    const getTotalCartItem = () => {
+        let totalItem = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = { getTotalCartItem, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
+    // console.log(cartItems);
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
